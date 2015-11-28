03ff1fc34c1ed6e6cfdc7a934d6d0e431ead3ae3
CD-Rom321 <bak_321@hotmail.com>
2015-11-27 22:34:47 -0800

    Changes were only made to Cache.java. It should be done and ready for testing.

--- Program_4/src/Cache.java
+++ Program_4/src/Cache.java
@@ -11,24 +11,21 @@ public class Cache {
         }
     }
 
-
     private class Entry {
-        private int blockId;        // data
-        private boolean reference;  // If true, data was used recently
-        private boolean dirty;      // If true, data was modified recently
+        private int blockId; // Identity in the page table.
+        private boolean reference; // If true, data was used recently.
+        private boolean dirty; // If true, data was modified recently.
 
         public Entry() {
             blockId = -1;
             reference = false;
             dirty = false;
         }
-
     }
 
     private Entry[] pageTable = null;
     private Vector<byte[]> cache;
-    private int victim;
-    // Done.
+
     private int findFreePage() {
         for (int i = 0; i < pageTable.length; i++) {
             if (pageTable[i].blockId == -1) {
@@ -37,7 +34,7 @@ public class Cache {
         }
         return -1;
     }
-    // Done.
+
     private int nextVictim() {
         for (int i = 0; i < pageTable.length; i = (i + 1) % pageTable.length) {
             if (pageTable[i].reference == false) {
@@ -49,54 +46,99 @@ public class Cache {
     }
 
     private void writeBack(int victimEntry) {
-
+        if (pageTable[victimEntry].blockId >= -1) {
+            SysLib.rawwrite(pageTable[victimEntry].blockId, cache.elementAt(victimEntry));
+            pageTable[victimEntry].dirty = false;
+        }
     }
 
     public synchronized boolean read(int blockId, byte buffer[]) {
-        if (blockId >= 0) {
+        if (blockId > -1) {
             // Searches for loop to check if blockID is in the page table.
             for (int i = 0; i < pageTable.length; i++) {
                 if (pageTable[i].blockId == blockId) {
-                    // Read from cache.
+                    System.arraycopy(cache.elementAt(i), 0, buffer, 0, buffer.length);
                     pageTable[i].reference = true;
                     return true;
                 }
             }
+            // BlockID is not in page table, so find a free page.
             int freePage = findFreePage();
-            // BlockID is not in page table, so find a free page
             if (freePage > -1) {
-                // Read from disk to cache.
-                // Read from cache.
-                pageTable[victim].reference = false;
+                SysLib.rawread(blockId, cache.elementAt(freePage));
+                System.arraycopy(cache.elementAt(freePage), 0, buffer, 0, buffer.length);
+                pageTable[freePage].blockId = blockId;
                 return true;
             }
-            victim = nextVictim(); // Probably don't need to store this outside of method.
-            if (pageTable[victim].dirty == true) {
-                writeBack(victim);
+            // No free page in page table, so find victim page.
+            int victim = nextVictim();
+            if (victim > -1) {
+                if (pageTable[victim].dirty == true) {
+                    writeBack(victim);
+                }
+                SysLib.rawread(blockId, cache.elementAt(victim));
+                System.arraycopy(cache.elementAt(victim), 0, buffer, 0, buffer.length);
+                pageTable[victim].blockId = blockId;
+                return true;
             }
-            // Read from disk to cache.
-            // Read from cache.
-            pageTable[victim].reference = false;
-
-            // Uses disk.read called from Kernel
-            SysLib.rawread(blockId, buffer);
-
-            return true;
         }
         return false;
     }
 
     public synchronized boolean write(int blockId, byte buffer[]) {
-        if (blockId >= 0) {
-            //Kernel.disk.write(blockId, buffer);
-            return true;
+        if (blockId > -1) {
+            // Searches for loop to check if blockID is in the page table.
+            for (int i = 0; i < pageTable.length; i++) {
+                if (pageTable[i].blockId == blockId) {
+                    if (pageTable[i].dirty == true) {
+                        writeBack(i);
+                    }
+                    System.arraycopy(buffer, 0, cache.elementAt(i), 0, buffer.length);
+                    pageTable[i].reference = true;
+                    pageTable[i].dirty = true;
+                    return true;
+                }
+            }
+            // BlockID is not in page table, so find a free page.
+            int freePage = findFreePage();
+            if (freePage > -1) {
+                System.arraycopy(buffer, 0, cache.elementAt(freePage), 0, buffer.length);
+                pageTable[freePage].blockId = blockId;
+                pageTable[freePage].dirty = true;
+                return true;
+            }
+            // No free page in page table, so find victim page.
+            int victim = nextVictim();
+            if (victim > -1) {
+                if (pageTable[victim].dirty == true) {
+                    writeBack(victim);
+                }
+                System.arraycopy(buffer, 0, cache.elementAt(victim), 0, buffer.length);
+                pageTable[victim].blockId = blockId;
+                pageTable[victim].dirty = true;
+                return true;
+            }
         }
         return false;
     }
 
     public synchronized void sync() {
+        for (int i = 0; i < pageTable.length; i++) {
+            if (pageTable[i].dirty == true) {
+                writeBack(i);
+            }
+        }
+        SysLib.sync();
     }
 
     public synchronized void flush() {
+        for (int i = 0; i < pageTable.length; i++) {
+            if (pageTable[i].dirty == true) {
+                writeBack(i);
+                pageTable[i].blockId = -1;
+                pageTable[i].reference = false;
+            }
+        }
+        SysLib.sync();
     }
 }
